d9168673844553847c03f491506ca354c5cdb7d9
Anonymous15052003 <sharonpro24x7@gmail.com>
2023-11-16 12:03:19 +0530

    modified username to SRN

--- stud_signup.php
+++ stud_signup.php
@@ -1,12 +1,12 @@
 <?php
 // Replace these credentials with your actual MySQL credentials
 $servername = "localhost";
-$username = "root";
+$SRN = "root";
 $password = "";
 $dbname = "college";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $SRN, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
@@ -15,10 +15,10 @@ if ($conn->connect_error) {
 
 // Process signup form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
+    $SRN = $_POST["SRN"];
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    $sql = "INSERT INTO users (SRN, password) VALUES ('$SRN', '$password')";
 
     if ($conn->query($sql) === TRUE) {
         echo "User registered successfully!";
